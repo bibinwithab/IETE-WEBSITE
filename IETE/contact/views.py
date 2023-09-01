a38041b2fb13ed343ee91724732d99ac975cc02f
bibinwithab <bibingraceson@gmail.com>
2023-09-02 00:46:25 +0530

    contact models update

--- IETE/contact/views.py
+++ IETE/contact/views.py
@@ -8,11 +8,4 @@ def contact(request):
     content = {
         'name':'CONTACT',
     }
-    if request == 'POST':
-        name = request.POST['name']
-        mail = request.POST['email']
-        message = request.POST['message']
-    
-        return HttpResponse("<h1>Thank You</h1>")
-
     return render(request, 'contact/contact.html', context=content)
