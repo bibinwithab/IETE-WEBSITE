ca18b2fb107f89a95279051db83a4e651bfa7e4c
bibinwithab <bibingraceson@gmail.com>
2023-09-02 01:02:40 +0530

    input from contact page is stored and displayed in admin page

--- IETE/contact/views.py
+++ IETE/contact/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from .models import Contact
 
 
 # Create your views here.
@@ -8,4 +9,13 @@ def contact(request):
     content = {
         'name':'CONTACT',
     }
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        message = request.POST['message']
+
+        Contact.objects.create(name=name, email=email, message=message)
+
+        return HttpResponse('<h1>thank-you. Your response is recorded</h1>')
+
     return render(request, 'contact/contact.html', context=content)
